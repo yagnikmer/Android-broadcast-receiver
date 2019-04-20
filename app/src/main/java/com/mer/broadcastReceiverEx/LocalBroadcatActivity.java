dbf0d56dbe125c3301cd2bbf72911f05a388c7da
Yagnik Mer <yagnikmer@gmail.com>
2019-04-20 13:24:01 +0530

    Add local broadcast receiver

---
+++ app/src/main/java/com/mer/broadcastReceiverEx/LocalBroadcatActivity.java
@@ -0,0 +1,54 @@
+package com.mer.broadcastReceiverEx;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+public class LocalBroadcatActivity extends Activity {
+
+
+    LocalBroadcastManager manager;
+    MyLocalBroadcaster localBroadcaster;
+    String FILTER = "com.mer.broadcasttrigger";
+    IntentFilter intentFilter = new IntentFilter(FILTER);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        manager = LocalBroadcastManager.getInstance(this);
+        localBroadcaster = new MyLocalBroadcaster();
+    }
+
+    protected void broadcastClick(View view) {
+        Intent intent = new Intent(FILTER);
+        intent.putExtra("msg", "Mer");
+        manager.sendBroadcast(intent);
+    }
+
+    class MyLocalBroadcaster extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d("AppFlow", intent.getStringExtra("msg"));
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        manager.registerReceiver(localBroadcaster, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        manager.unregisterReceiver(localBroadcaster);
+    }
+}
